b893df6ee42767fd6837887cb0979deea481dbbf
martinasorialanzi <martinasorialanzi@gmail.com>
2024-01-02 22:12:22 -0300

    section project finished

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,51 +1,106 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "../projects/projects.css";
-import { Container, Row, Col, Card, Button, ListGroup } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Button,
+  ListGroup,
+  Badge,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Slider from "react-slick";
-import projectData from "../../assets/projects.json"
+import projectData from "../../assets/projects.json";
 
 const Projects = () => {
   const settings = {
     dots: true,
-    infinite: false,
+    infinite: true,
+    speed: 500,
     slidesToShow: 2,
     slidesToScroll: 1,
-    vertical: true,
-    verticalSwiping: true,
-    swipeToSlide: true,
-
-    beforeChange: function(currentSlide, nextSlide) {
-      console.log("before change", currentSlide, nextSlide);
-    },
-    afterChange: function(currentSlide) {
-      console.log("after change", currentSlide);
-    }
+    initialSlide: 0,
+    centerMode: true,
+    centerPadding: "20px",
+    responsive: [
+   
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
   return (
     <>
       <Container className="containerProjects">
         <Row className="p-0 rowProjects">
-          <Col className="colTittleProjects "xl={6} md={6} >
+          <Col className="colTittleProjects " xl={6} md={12}>
             <p className="tittleProjects">Projects</p>
           </Col>
-          <Col className="colProjects justify-content-center" xl={6} md={6}>
+          <Col className="colProjects justify-content-center" xl={6} md={12}>
             <Slider {...settings} className=" slider mx-4 p-1 ">
-              {projectData.projects.map((project)=>(
-                <Card className="card" >
-                <Card.Body>
-                  <Card.Title className="cardTittle">{project.title}</Card.Title>
-                  <Card.Subtitle className="mb-2 cardSubtittle">
-                   {project.description}
-                  </Card.Subtitle>
-                  <Card.Text></Card.Text>
-                  <Card.Link className="cardLink" href={project.weblink}>Web Link</Card.Link>
-                  <Card.Link className="cardLink" href={project.github}>GitHub</Card.Link>
-                </Card.Body>
-              </Card>
+              {projectData.projects.map((project) => (
+                <Card className="card" key={project._id}>
+                  <Card.Img
+                    variant="top"
+                    src={project.image}
+                    className="image"
+                  />
+                  <div className=" divBadge d-flex align-items-center justify-content-center flex-wrap">
+                     {project.technologies.map((technologie) => (
+                      <Badge className="badgeTech" key={project._id}>
+                        {technologie}
+                      </Badge>
+                    ))}
+</div>
+                  <Card.Body className="d-flex align-items-center justify-content-center flex-wrap">
+                    <Card.Title className="cardTittle">
+                      {project.title}
+                    </Card.Title>
+
+                 <div className="divDescription d-flex align-items-center justify-content-center flex-wrap">
+                    <Card.Text className="description">{project.description}</Card.Text>
+                    </div>
+                  </Card.Body>
+                  <Card.Footer>
+                    <Card.Link
+                      className="cardLink"
+                      href={project.weblink}
+                      target="_blank"
+                    >
+                      Web Link
+                    </Card.Link>
+                    <Card.Link
+                      className="cardLink"
+                      href={project.github}
+                      target="_blank"
+                    >
+                      GitHub
+                    </Card.Link>
+                  </Card.Footer>
+                </Card>
               ))}
-              
-       
             </Slider>
           </Col>
         </Row>
