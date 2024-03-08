6c767d145a8f8fd452f853bd23f596550e48b672
martinasorialanzi <martinasorialanzi@gmail.com>
2024-03-08 11:28:10 -0300

    add skills

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import "../about/about.css";
 // import fotCv from "../../assets/img/fotCv.jpg"
 import { Link } from "react-router-dom";
-import pdf from "../../assets/cv/ResumeMartinaSoriaLanzi.pdf";
+import pdf from "../../assets/cv/ResumeMartianSoriaLanzi.pdf";
 
 const About = () => {
   return (
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "../footer/footer.css";
-import pdf from "../../assets/cv/ResumeMartinaSoriaLanzi.pdf";
+import pdf from "../../assets/cv/ResumeMartianSoriaLanzi.pdf";
 
 const Footer = () => {
   return (
